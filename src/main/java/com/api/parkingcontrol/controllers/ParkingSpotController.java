d9f02079f9244c48640f100e915d467dd25ff711
Jander Gustavo <jander.gustavo@gmail.com>
2023-10-01 15:28:33 -0300

    Adicionando spring-security / validando usuário e roles (controles de acesso)

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
 
